6a1574dda86e7afa790d6e0a1709f84479f25be5
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-10-16 12:41:27 -0400

    added wnc hiking hub card and removed two website cards

--- src/assets/cards.ts
+++ src/assets/cards.ts
@@ -1,83 +1,6 @@
 export default [
   {
     id: 0,
-    title: "Ravenous",
-    image: "images/ravenous.png",
-    technology: "HTML, CSS, TypeScript, React",
-    summary: "This site uses a Yelp API to generate restaurant suggestions based on location and keyword.",
-    gitHubLink: "https://github.com/AnnieMcMahon/ravenous-v2",
-    gitHubTitle: "ravenous",
-    webPageLink: "https://ravenous-v2.vercel.app/",
-    webPageTitle: "ravenous-api-annie"
-  },
-  {
-    id: 1,
-    title: "Jammming",
-    image: "images/jammming.png",
-    technology: "HTML, CSS, JavaScript, React",
-    summary: "This site uses a Spotify API to allow users to find songs based on search term, add songs to a new playlist, and save the new playlist on their Spotify account.",
-    gitHubLink: "https://github.com/AnnieMcMahon/jammming",
-    gitHubTitle: "jammming",
-    webPageLink: "https://annie-jammming.netlify.app/",
-    webPageTitle: "annie-jammming"
-  },
-  {
-    id: 2,
-    title: "Minimal Reddit",
-    image: "images/reddit.png",
-    technology: "HTML, CSS, JavaScript, React/Redux",
-    summary: "The application allows users to view and search posts and comments provided by the Reddit API.",
-    gitHubLink: "https://github.com/AnnieMcMahon/minimal-reddit",
-    gitHubTitle: "minimal-reddit",
-    webPageLink: "https://minimal-reddit-annie.netlify.app/",
-    webPageTitle: "minimal-reddit-annie"
-  },
-  {
-    id: 3,
-    title: "Film Finder",
-    image: "images/film-finder.png",
-    technology: "HTML, CSS, JavaScript",
-    summary: "This is a project I had to do for Codecademy where users can get a random movie recommendation by selecting a genre. Some of the code was provided, but I completed it and modified it to make it my own.",
-    gitHubLink: "https://github.com/AnnieMcMahon/film-finder",
-    gitHubTitle: "film-finder",
-    webPageLink: "https://anniemcmahon.github.io/film-finder/",
-    webPageTitle: "film-finder"
-   },
-   {
-    id: 4,
-    title: "HTML Cheatsheet",
-    image: "images/html-cheatsheet.png",
-    technology: "HTML, CSS",
-    summary: "Here are my notes about HTML5 and some examples. I developed this project to practice HTML and CSS and to have all my notes in one place.",
-    gitHubLink: "https://github.com/AnnieMcMahon/HTML-cheatsheet",
-    gitHubTitle: "HTML-cheatsheet",
-    webPageLink: "https://anniemcmahon.github.io/HTML-cheatsheet/",
-    webPageTitle: "HTML-cheatsheet"
-   },
-   {
-    id: 5,
-    title: "JavaScript Cheatsheet",
-    image: "images/js-cheatsheet.png",
-    technology: "HTML, CSS, JavaScript",
-    summary: "This is a place where I keep my notes about JavaScript (JS6). I developed this project to practice my new coding skills and to have all my notes in one place.",
-    gitHubLink: "https://github.com/AnnieMcMahon/JavaScript-cheatsheet",
-    gitHubTitle: "JavaScript-cheatsheet",
-    webPageLink: "https://anniemcmahon.github.io/JavaScript-cheatsheet/",
-    webPageTitle: "JavaScript-cheatsheet"
-   },
-   {
-    id: 6,
-    title: "Fifteen JavaScript Projects",
-    image: "images/fifteen-js-projects.png",
-    technology: "HTML, CSS, JavaScript",
-    summary: "I used starter code and step-by-step video from John Smilga to create fifteen vanilla JavaScript projects.",
-    gitHubLink: "https://github.com/AnnieMcMahon/fifteen-js-projects",
-    gitHubTitle: "fifteen-js-projects",
-    webPageLink: "https://anniemcmahon.github.io/fifteen-js-projects/",
-    webPageTitle: "fifteen-js-projects"
-   },
-   {
-    id: 7,
     title: "Mini-Games",
     image: "images/mini-games.png",
     technology: "CSS, JavaScript, React, Next.JS",
@@ -88,25 +11,91 @@ export default [
     webPageTitle: "mini-games-v2.vercel.app"
    },
    {
-    id: 8,
-    title: "Company Website",
-    image: "images/company.png",
-    technology: "HTML, CSS",
-    summary: "I built a fictitious company's website using a flexbox layout.",
-    gitHubLink: "https://github.com/AnnieMcMahon/remodeling",
-    gitHubTitle: "remodeling",
-    webPageLink: "https://anniemcmahon.github.io/remodeling/",
-    webPageTitle: "remodeling"
+    id: 1,
+    title: "WNC Hiking Hub",
+    image: "images/wnc-hiking-hub.png",
+    technology: "CSS, JavaScript, React, NextJS",
+    summary: "This app will allow users to create a free account, post a hike for others to join, or join a posted hike. (WIP)",
+    gitHubLink: "https://github.com/AnnieMcMahon/wnc-hiking-hub-v2",
+    gitHubTitle: "wnc-hiking-hub-v2",
+    webPageLink: "https://wnc-hiking-hub-v2.vercel.app/",
+    webPageTitle: "wnc-hiking-hub-v2.vercel.app"
+   },
+  {
+    id: 2,
+    title: "Ravenous",
+    image: "images/ravenous.png",
+    technology: "HTML, CSS, TypeScript, React",
+    summary: "This site uses a Yelp API to generate restaurant suggestions based on location and keyword.",
+    gitHubLink: "https://github.com/AnnieMcMahon/ravenous-v2",
+    gitHubTitle: "ravenous",
+    webPageLink: "https://ravenous-v2.vercel.app/",
+    webPageTitle: "ravenous-api-annie"
+  },
+  {
+    id: 3,
+    title: "Jammming",
+    image: "images/jammming.png",
+    technology: "HTML, CSS, JavaScript, React",
+    summary: "This site uses a Spotify API to allow users to find songs based on search term, add songs to a new playlist, and save the new playlist on their Spotify account.",
+    gitHubLink: "https://github.com/AnnieMcMahon/jammming",
+    gitHubTitle: "jammming",
+    webPageLink: "https://annie-jammming.netlify.app/",
+    webPageTitle: "annie-jammming"
+  },
+  {
+    id: 4,
+    title: "Minimal Reddit",
+    image: "images/reddit.png",
+    technology: "HTML, CSS, JavaScript, React/Redux",
+    summary: "The application allows users to view and search posts and comments provided by the Reddit API.",
+    gitHubLink: "https://github.com/AnnieMcMahon/minimal-reddit",
+    gitHubTitle: "minimal-reddit",
+    webPageLink: "https://minimal-reddit-annie.netlify.app/",
+    webPageTitle: "minimal-reddit-annie"
+  },
+  {
+    id: 5,
+    title: "Film Finder",
+    image: "images/film-finder.png",
+    technology: "HTML, CSS, JavaScript",
+    summary: "This is a project I had to do for Codecademy where users can get a random movie recommendation by selecting a genre. Some of the code was provided, but I completed it and modified it to make it my own.",
+    gitHubLink: "https://github.com/AnnieMcMahon/film-finder",
+    gitHubTitle: "film-finder",
+    webPageLink: "https://anniemcmahon.github.io/film-finder/",
+    webPageTitle: "film-finder"
    },
    {
-    id: 9,
-    title: "Club Website",
-    image: "images/club.png",
+    id: 6,
+    title: "HTML Cheatsheet",
+    image: "images/html-cheatsheet.png",
     technology: "HTML, CSS",
-    summary: "I created a fictitious club website using responsive design tools.",
-    gitHubLink: "https://github.com/AnnieMcMahon/club",
-    gitHubTitle: "club",
-    webPageLink: "https://anniemcmahon.github.io/club/",
-    webPageTitle: "club"
-   }
+    summary: "Here are my notes about HTML5 and some examples. I developed this project to practice HTML and CSS and to have all my notes in one place.",
+    gitHubLink: "https://github.com/AnnieMcMahon/HTML-cheatsheet",
+    gitHubTitle: "HTML-cheatsheet",
+    webPageLink: "https://anniemcmahon.github.io/HTML-cheatsheet/",
+    webPageTitle: "HTML-cheatsheet"
+   },
+   {
+    id: 7,
+    title: "JavaScript Cheatsheet",
+    image: "images/js-cheatsheet.png",
+    technology: "HTML, CSS, JavaScript",
+    summary: "This is a place where I keep my notes about JavaScript (JS6). I developed this project to practice my new coding skills and to have all my notes in one place.",
+    gitHubLink: "https://github.com/AnnieMcMahon/JavaScript-cheatsheet",
+    gitHubTitle: "JavaScript-cheatsheet",
+    webPageLink: "https://anniemcmahon.github.io/JavaScript-cheatsheet/",
+    webPageTitle: "JavaScript-cheatsheet"
+   },
+   {
+    id: 8,
+    title: "Fifteen JavaScript Projects",
+    image: "images/fifteen-js-projects.png",
+    technology: "HTML, CSS, JavaScript",
+    summary: "I used starter code and step-by-step video from John Smilga to create fifteen vanilla JavaScript projects.",
+    gitHubLink: "https://github.com/AnnieMcMahon/fifteen-js-projects",
+    gitHubTitle: "fifteen-js-projects",
+    webPageLink: "https://anniemcmahon.github.io/fifteen-js-projects/",
+    webPageTitle: "fifteen-js-projects"
+   },
 ];
